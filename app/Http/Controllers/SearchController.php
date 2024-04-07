c1a4e16f19e397a2cbe76a3a581d3e7d1f20f1ed
Martin <martin.birisic@gmail.com>
2024-04-07 17:03:43 +0200

    Added counting of positive and negative results for the given word, as well as calculation of the final score.

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,15 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+use function Laravel\Prompts\text;
 
 
 class SearchController extends Controller
 {
     public function getWordPopularity($word, $platform = "github")
     {
-        // validate word
+        //validate word
 
-        if ($platform === "github") { // use enum
+        if ($platform === "github") { //use enum
             $authorizationToken = env("GITHUB_PERSONAL_ACCESS_TOKEN");
             $endpoint = "https://api.github.com/search/issues";
             $user = "birisic";
@@ -33,11 +34,61 @@ class SearchController extends Controller
                     throw new \Exception("Response was not set.");
                 }
 
+                //separate into a function
                 $contentType = $response->header('Content-Type');
-                $output = [];
-                foreach ($response->json()["items"] as $item) {
-                    $output[] = $item["text_matches"];
+//                return $response;//response()->json($response)->header('Content-Type', $contentType);
+
+                $strings = [];
+                foreach ($response->json()["items"] as $issue) {
+                    foreach ($issue["text_matches"] as $textMatch) {
+                        $text = strtolower(str_replace(["\n", "\t", ""], "", $textMatch["fragment"]));
+//                        $strings[] = $text;
+
+                        if (preg_match("/\b(?:php\s*(rocks|sucks))\b/i", $text)){
+                            $strings[] = $text;
+                        }
+                    }
                 }
+
+                $counterPositive = 0;
+                $counterNegative = 0;
+                $arrOfWords = [];
+                foreach ($strings as $string) {
+                    $words = array_filter(explode(" ", $string)); //remove extra spaces
+                    $arrOfWords[] = array_values($words); //use only values
+                }
+
+//                return $arrOfWords;
+
+                foreach ($arrOfWords as $words) {
+                    foreach ($words as $key=>$value) {
+                        if ($value === $word){
+                            if ($words[$key + 1] === "rocks"){
+                                $counterPositive++;
+                            }
+                            else if ($words[$key + 1] === "sucks"){
+                                $counterNegative++;
+                            }
+                        }
+                    }
+                }
+
+                $counterTotal = $counterPositive + $counterNegative;
+
+                if ($counterTotal != 0) {
+                    $score = ($counterPositive / $counterTotal) * 10;
+                }
+                else {
+                    $score = 0;
+                }
+
+                $output = [
+                    "term" => $word,
+                    "positiveCount" => $counterPositive,
+                    "negativeCount" => $counterNegative,
+                    "total" => $counterTotal,
+                    "score" => round($score, 2)
+                ];
 
                 return response()->json($output)->header('Content-Type', $contentType);
             }
